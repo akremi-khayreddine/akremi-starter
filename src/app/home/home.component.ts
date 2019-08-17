cc8f81c58ff537cf157d6dfe79ff4ee02670e76f
khayreddine <khayreddine.akremi@gmail.com>
2019-08-17 22:23:14 +0100

    Second commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild("stickyMenu", { static: true }) menuElement: ElementRef;
 
-  constructor() { }
+  sticky = false;
+  elementPosition: any;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
   }
 
+  @HostListener("window:scroll", ["$event"])
+  handleScroll() {
+    const windowScroll = window.pageYOffset;
+    if (windowScroll >= 500) {
+      this.sticky = true;
+    } else {
+      this.sticky = false;
+    }
+  }
 }
