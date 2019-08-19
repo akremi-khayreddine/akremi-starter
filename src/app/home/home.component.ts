9739279bf2bb95b0f55be4b900fd69d39f582b45
khayreddine <khayreddine.akremi@gmail.com>
2019-08-19 23:32:34 +0100

    h

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,9 @@ import {
   ElementRef,
   AfterViewInit
 } from "@angular/core";
-
+import { FormGroup, FormControl } from "@angular/forms";
+import { tileLayer, latLng, circle, polygon } from "leaflet";
+import * as L from 'leaflet';
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -18,9 +20,56 @@ export class HomeComponent implements OnInit, AfterViewInit {
   sticky = false;
   elementPosition: any;
 
+  form: FormGroup;
+
+  layersControl = {
+    baseLayers: {
+      "Open Street Map": tileLayer(
+        "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        { maxZoom: 18, attribution: "..." }
+      ),
+      "Open Cycle Map": tileLayer(
+        "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+        { maxZoom: 18, attribution: "..." }
+      )
+    },
+    overlays: {
+      "Big Circle": circle([46.95, -122], { radius: 5000 }),
+      "Big Square": polygon([
+        [46.8, -121.55],
+        [46.9, -121.55],
+        [46.9, -121.7],
+        [46.8, -121.7]
+      ])
+    }
+  };
+  // Open Street Map Definition
+  LAYER_OSM = {
+    id: "openstreetmap",
+    name: "Open Street Map",
+    enabled: false,
+    layer: L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 18,
+      attribution: "Open Street Map"
+    })
+  };
+  // Values to bind to Leaflet Directive
+  layersControlOptions = { position: "bottomright" };
+  baseLayers = {
+    "Open Street Map": this.LAYER_OSM.layer
+  };
+  options = {
+    zoom: 13,
+    center: L.latLng([46.879966, -121.726909])
+  };
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null)
+    });
+  }
 
   ngAfterViewInit() {
     this.elementPosition = this.menuElement.nativeElement.offsetTop;
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,23 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/material";
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LeafletModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [HomeComponent]
 })
